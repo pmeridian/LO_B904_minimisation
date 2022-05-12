240adf58c3b4b52dfbdee0e2d3cb9d702963d3af
Paolo Meridiani <paolo.meridiani@cern.ch>
2022-05-12 02:27:27 +0200

    fixes

--- minimize.sh
+++ minimize.sh
@@ -6,7 +6,8 @@ python minimize.py --run_ori 410_HPK_CH09_OV1.0_Tm30_run160 --run_target 479_HPK
 
 #OV 1.2
 python minimize.py --run_ori 410_HPK_CH09_OV1.2_Tm30_run161 --run_target 479_HPK_CH08_OV1.2_Tm40_run167 --norm 6.4 --scale 0.765 --smear 370
-python minimize.py --run_ori 410_HPK_CH09_OV1.2_Tm30_run161 --run_target 479_HPK_CH10_OV1.2_Tm40_run171 --norm 6.4 --scale 0.765 --smear 370
+#Bad RUN
+#python minimize.py --run_ori 410_HPK_CH09_OV1.2_Tm30_run161 --run_target 479_HPK_CH10_OV1.2_Tm40_run171 --norm 6.4 --scale 0.765 --smear 370
 
 #OV0.8
 python minimize.py --run_ori 410_HPK_CH09_OV0.8_Tm30_run159 --run_target 479_HPK_CH08_OV0.8_Tm40_run165 --norm 6.2 --scale 0.84 --smear 185
